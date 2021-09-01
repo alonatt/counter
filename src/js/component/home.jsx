09d85d23f4491eddd07c52c62456fc4191cd6339
Shalooko <gonzalo.vergara.t@gmail.com>
2021-09-01 20:36:04 +0000

    simplecounterOK

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+/*const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
@@ -21,6 +21,21 @@ const Home = () => {
 			</p>
 		</div>
 	);
-};
+};*/
 
-export default Home;
+/*function Home (props) {
+	return (
+		<div className="texte-center mt-5">
+			<a href="#" className="btn btn-succes">{props.decena}</a>
+			<a href="#" className="btn btn-succes">{props.unidad}</a>
+		</div>
+	);
+}
+
+Home.propTypes = {
+	decena: propTypes.numbers,
+	unidad: propTypes.numbers
+};
+export default Home;*/
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -9,7 +9,53 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import Home from "./component/home.jsx";
+//import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+function SecondsCounter(props) {
+	return (
+		<div className="contador">
+			<div>
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="seis">{props.digitoseis % 10}</div>
+			<div className="cinco">{props.digitocinco % 10}</div>
+			<div className="cuatro">{props.digitocuatro % 10}</div>
+			<div className="tres">{props.digitotres % 10}</div>
+			<div className="dos">{props.digitodos % 10}</div>
+			<div className="uno">{props.digitouno % 10}</div>
+		</div>
+	);
+}
+SecondsCounter.propTypes = {
+	digitouno: PropTypes.numbers,
+	digitodos: PropTypes.numbers,
+	digitotres: PropTypes.numbers,
+	digitocuatro: PropTypes.numbers,
+	digitocinco: PropTypes.numbers,
+	digitoseis: PropTypes.numbers
+};
+
+let counter = 0;
+setInterval(function() {
+	const uno = Math.floor(counter / 1);
+	const dos = Math.floor(counter / 10);
+	const tres = Math.floor(counter / 100);
+	const cuatro = Math.floor(counter / 1000);
+	const cinco = Math.floor(counter / 10000);
+	const seis = Math.floor(counter / 100000);
+
+	counter++;
+	ReactDOM.render(
+		<SecondsCounter
+			digitouno={uno}
+			digitodos={dos}
+			digitotres={tres}
+			digitocuatro={cuatro}
+			digitocinco={cinco}
+			digitoseis={seis}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
